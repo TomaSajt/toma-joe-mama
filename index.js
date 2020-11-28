3226ba29af7a9deab7bf9f9f3ac9750a23fc0f71
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2020-11-28 19:28:50 +0100

    ASD

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 ﻿const Discord = require('discord.js');
 const config = require('./config.json');
-var crashing = false;
 
 var client = new Discord.Client();
 client.once('ready', () => {
     canRestart = true;
     console.log('Restarted');
+
+
 });
 client.on('message', async message => {
     if (message.author.bot) return;
@@ -21,28 +22,24 @@ client.on('message', async message => {
     if (message.content.toLowerCase().includes("joe") && (message.content.toLowerCase().includes("who's") || message.content.toLowerCase().includes("who is") || message.content.toLowerCase().includes("whos") || message.content.toLowerCase().includes("who s"))) {
         message.channel.send("joe mama")
     }
-    if (!message.content.startsWith(config.prefix)) return;
-    const args = message.content.slice(config.prefix.length).trim().split(' ').filter(str => str != "");
-    switch (args[0]) {
-        case "crash":
-            if (message.member.id == "436579592447197225") {
-                if (!crashing) {
-                    crashing = true;
-                    message.channel.send("crashing in 3")
-                    await delay(1000)
-                    message.channel.send("crashing in 2")
-                    await delay(1000)
-                    message.channel.send("crashing in 1")
-                    await delay(1000)
-                    message.channel.send("crashed")
-                    await delay(100)
-                    crash;
-                }
-            } else {
-                message.channel.send("only TomaSajt can do that")
-            }
-            break;
+    //const args = message.content.slice(config.prefix.length).trim().split(' ').filter(str => str != "");
+    if (message.content.toLowerCase() == "joe!crash") {
+        if (message.member.id == "436579592447197225") {
+            crashing = true;
+            message.channel.send("crashing in 3")
+            await delay(1000)
+            message.channel.send("crashing in 2")
+            await delay(1000)
+            message.channel.send("crashing in 1")
+            await delay(1000)
+            message.channel.send("crashed")
+            await delay(100)
+            crash;
+        } else {
+            message.channel.send("only TomaSajt can do that")
+        }
     }
+
 });
 client.login(config.token);
 
