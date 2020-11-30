97fe74971a7178fbe3ddf5c506db42ba6511f146
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2020-11-30 09:59:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
 ﻿const Discord = require('discord.js');
 const config = require('./config.json');
+var crashing = false;
 
 var client = new Discord.Client();
 client.once('ready', () => {
-    canRestart = true;
     console.log('Restarted');
-
-
 });
+
 client.on('message', async message => {
     if (message.author.bot) return;
-    if (message.content.toLowerCase().includes("karesz")) {
-        message.react("<:karesz:776413600117030913>")
-    }
-    if (message.content.toLowerCase().includes("pog")) {
-        message.react("<:RuviPog:777804089290784808>")
-    }
-    if (message.content.toLowerCase().includes("joe mama")) {
-        message.channel.send(`> You are almost as funny as me ${message.author}`, { files: [randomPicture()] })
-    }
-    if (message.content.toLowerCase().includes("joe") && (message.content.toLowerCase().includes("who's") || message.content.toLowerCase().includes("who is") || message.content.toLowerCase().includes("whos") || message.content.toLowerCase().includes("who s"))) {
-        message.channel.send("joe mama")
-    }
-    //const args = message.content.slice(config.prefix.length).trim().split(' ').filter(str => str != "");
-    if (message.content.toLowerCase() == "joe!crash") {
-        if (message.member.id == "436579592447197225") {
-            crashing = true;
-            message.channel.send("crashing in 3")
-            await delay(1000)
-            message.channel.send("crashing in 2")
-            await delay(1000)
-            message.channel.send("crashing in 1")
-            await delay(1000)
-            message.channel.send("crashed")
-            await delay(100)
-            crash;
-        } else {
-            message.channel.send("only TomaSajt can do that")
+    if (!crashing) {
+        if (message.content.toLowerCase().includes("karesz")) {
+            message.react("<:karesz:776413600117030913>")
+        }
+        if (message.content.toLowerCase().includes("pog")) {
+            message.react("<:RuviPog:777804089290784808>")
+        }
+        if (message.content.toLowerCase().includes("joe mama")) {
+            message.channel.send(`> You are almost as funny as me ${message.author}`, { files: [randomPicture()] })
+        }
+        if (message.content.toLowerCase().includes("joe") && (message.content.toLowerCase().includes("who's") || message.content.toLowerCase().includes("who is") || message.content.toLowerCase().includes("whos") || message.content.toLowerCase().includes("who s"))) {
+            message.channel.send("joe mama")
+        }
+        //const args = message.content.slice(config.prefix.length).trim().split(' ').filter(str => str != "");
+        if (message.content.toLowerCase() == "joe!crash") {
+            if (message.member.id == "436579592447197225") {
+                crashing = true;
+                message.channel.send("crashing in 3")
+                await delay(1000)
+                message.channel.send("crashing in 2")
+                await delay(1000)
+                message.channel.send("crashing in 1")
+                await delay(1000)
+                message.channel.send("crashed")
+                await delay(100)
+                crash;
+            } else {
+                message.channel.send("only TomaSajt can do that")
+            }
         }
     }
-
 });
 client.login(config.token);
 
