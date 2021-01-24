96e22a026d5b2431a2f28f590530320b2cfbbb1c
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2021-01-24 22:48:46 +0100

    google image but without api
    this code ****ing sucks

--- index.js
+++ index.js
@@ -58,6 +58,50 @@ client.on('message', async message => {
             })
 
     }
+    if (message.content.toLowerCase().startsWith(`${config.prefix}search`)) {
+        var args = message.content.substring(`${config.prefix}search`.length).trim().split().filter(str => str != "");
+        if (args.length > 1) {
+            message.channel.send("Too many arguments")
+        } else {
+            var n = 1;
+            if (args.length == 1) {
+                n = parseInt(args[0])
+            }
+            if (n < 1) {
+                message.channel.send("Please give a positive number")
+            } else {
+                fetch("http://www.google.com/search?q=joe+mama&tbm=isch").then(res => res.text()).then(str => {
+                    var strings = []
+                    var search = "<img class=\"t0fcAb\" alt=\"\" src=\"";
+                    var stringsFound = 0;
+                    var searchChar = search.length;
+                    while (stringsFound < n && searchChar <= str.length) {
+                        if (str.substring(searchChar - search.length, searchChar) == search) {
+                            var endSearch = 0;
+                            while (str.substring(searchChar + endSearch, searchChar + endSearch + 4) != "&amp") {
+                                endSearch++;
+                            }
+                            strings.push(str.substring(searchChar, searchChar + endSearch));
+                            stringsFound++;
+                        }
+                        searchChar++;
+                    }
+                    var toSend = []
+                    var j = 0;
+                    for (var i = 0; i < strings.length; i++) {
+                        if ((toSend[j] + strings[i] + "\n").length > 2000) {
+                            j++;
+                        }
+                        toSend[j] += strings[i] + "\n"
+                    }
+                    message.channel.send(`Here are the first ${n} pictures`)
+                    for (var i = 0; i < toSend.length; i++) {
+                        message.channel.send(toSend[i])
+                    }
+                });
+            }
+        }
+    }
     if (message.content.toLowerCase() == `${config.prefix}crash`) {
         if (message.memsber.id == "436579592447197225") {
             crashing = true;
@@ -72,13 +116,6 @@ client.on('message', async message => {
             crash;
         } else {
             message.channel.send("only TomaSajt can do that")
-        }
-    }
-    if (message.member.id == "780366371083124746") {
-        if (message.content.toLowerCase().includes("i am mom")) {
-            if (Math.floor(Math.random() * 5) == 0) {
-                message.channel.send("I am the real mother, not you, <@780366371083124746>")
-            }
         }
     }
 });
