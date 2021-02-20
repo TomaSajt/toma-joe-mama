ce9678cf0743f24be86028ad4e0ace6f49db6b10
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2021-02-20 15:39:40 +0100

    Update play.js

--- commands/normal/async/play.js
+++ commands/normal/async/play.js
@@ -5,11 +5,15 @@ module.exports = async (message, client) => {
     if (message instanceof Message) {
         var text = message.content.toLowerCase()
         if (text.startsWith(`${config.prefix}play`)) {
-            var args = message.content.substring(`${config.prefix}play`.length).trim().split().filter(str => str != "");
-            var voiceConnection = await message.member.voice.channel.join();
-            const broadcast = client.voice.createBroadcast();
-            broadcast.play(await ytdl('https://www.youtube.com/watch?v=dQw4w9WgXcQ'), { type: 'opus' });
-            voiceConnection.play(broadcast);
+            try {
+                var args = message.content.substring(`${config.prefix}play`.length).trim().split().filter(str => str != "");
+                var voiceConnection = await message.member.voice.channel.join();
+                const broadcast = client.voice.createBroadcast();
+                broadcast.play(await ytdl('https://www.youtube.com/watch?v=dQw4w9WgXcQ'), { type: 'opus' });
+                voiceConnection.play(broadcast);
+            } catch (e) {
+                console.log(e);
+            }
         }
     }
 }
